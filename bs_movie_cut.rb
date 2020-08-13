ad076be5f0754a4538f8e99d2bf51ec53c115619
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2020-08-13 13:59:01 +0900

    誤記修正

--- bs_movie_cut.rb
+++ bs_movie_cut.rb
@@ -48,7 +48,7 @@ require 'json'
 #ERR_LOG ・・・ エラーログファイル名
 
 #ソフトバージョン
-SOFT_VER        = '2020/08/12'
+SOFT_VER        = '2020/08/13'
 APP_VER_COOMENT = "BeatSaber Movie Cut TOOL Ver#{SOFT_VER}\r\n for ActiveScriptRuby(1.8.7-p330)\r\nCopyright 2020 リュナン [Rynan] (Twitter @rynan4818)"
 
 #設定ファイル
@@ -89,7 +89,7 @@ DEFALUT_PREVIEW_FFMPEG = " -c copy -async 1"
 DEFALUT_PREVIEW_KEYCUT = false
 DEFAULT_SUBTITLE_FILE  = EXE_DIR + "subtitle_temp.mp4"
 DEFAULT_FFMPEG_OPTION  = ["#DEFALUT#  -c copy -async 1",
-                          "#Twitter 2kbps#  -r 40 -c:v libx264 -pix_fmt yuv420p -strict -2 -c:a aac -b:v 1878k -minrate 1878k -maxrate 1878k -bufsize 1878k -b:a 128k -async 1 -vsync cfr -s 1280x720",
+                          "#Twitter 2Mbps#  -r 40 -c:v libx264 -pix_fmt yuv420p -strict -2 -c:a aac -b:v 1878k -minrate 1878k -maxrate 1878k -bufsize 1878k -b:a 128k -async 1 -vsync cfr -s 1280x720",
                           "#Twitter 15.5Mbps#  -c:v libx264 -pix_fmt yuv420p -strict -2 -c:a aac -b:a 512k -b:v 15360k -async 1",
                           "#NVENC_Acopy#  -r 60 -vsync cfr -async 1 -c:a copy -c:v h264_nvenc -b:v 22M","#NO COPY#  -async 1"]
 DEFAULT_OUT_FILE_NAME  = ['#DEFALUT#  #{time_name}_#{cleared}_#{songName}_#{levelAuthorName}_#{difficulty}_#{rank}_#{scorePercentage}%_#{miss}.mp4',
