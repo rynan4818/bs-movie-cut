b74ff1b94f3868bcfa0c8e8cce8fe8645c49b870
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2023-06-10 10:30:50 +0900

    VersionUP

--- bs_movie_cut.rb
+++ bs_movie_cut.rb
@@ -50,7 +50,7 @@ require 'json'
 $winshell  = WIN32OLE.new("WScript.Shell")
 
 #ソフトバージョン
-SOFT_VER        = '2023/05/13'
+SOFT_VER        = '2023/06/10'
 APP_VER_COOMENT = "BeatSaber Movie Cut TOOL Ver#{SOFT_VER}\r\n for ActiveScriptRuby(1.8.7-p330)\r\nCopyright 2020 リュナン [Rynan] (Twitter @rynan4818)"
 
 #設定ファイル
